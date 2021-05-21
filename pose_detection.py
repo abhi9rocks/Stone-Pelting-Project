ae115521db3b1fb5cf6b61a582dc803b9009c6dc
Abhinav Singh <aks.singh774@gmail.com>
2021-05-21 19:12:33 +0530

    python file added

--- pose_detection.py
+++ pose_detection.py
@@ -1,12 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # 0. Install and Import Dependencies
-
-# In[6]:
 
 
-get_ipython().system('pip install mediapipe opencv-python')
+
 
 
 # In[7]:
@@ -23,16 +17,6 @@ mp_pose = mp.solutions.pose
 
 
 # VIDEO FEED
-cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
-    cv2.imshow('Mediapipe Feed', frame)
-    
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-        
-cap.release()
-cv2.destroyAllWindows()
 
 
 # # 1. Make Detections
@@ -40,42 +24,9 @@ cv2.destroyAllWindows()
 # In[8]:
 
 
-cap = cv2.VideoCapture(0)
-## Setup mediapipe instance
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-      
-        # Make detection
-        results = pose.process(image)
-    
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
-        # Render detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
-                                 )               
-        
-        cv2.imshow('Mediapipe Feed', image)
-
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
 
 
-# In[11]:
 
-
-get_ipython().run_line_magic('pinfo2', 'mp_drawing.DrawingSpec')
 
 
 # # 2. Determining Joints
